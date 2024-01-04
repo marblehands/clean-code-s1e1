946e210ebc18be90ad3ec1728bd26b40b445b77b
Anna Chebysheva <anna.chebysheva@gmail.com>
2024-01-04 22:10:38 +0100

    refactor(basic-3.4): replace tag selectors except for resetting the default styles

--- app.js
+++ app.js
@@ -32,19 +32,23 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
 
+    listItem.className = 'list-item';
+
     label.innerText=taskString;
-    label.className='task';
+    label.className='task-label';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.className="input-checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="input-text";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="btn edit";
 
-    deleteButton.className="delete";
+    deleteButton.className="btn delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className = 'delete-icon';
     deleteButton.appendChild(deleteButtonImg);
 
 
